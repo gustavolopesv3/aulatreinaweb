f8fff78b73cac0e83f093494d8c52474090a9c52
gustavolopesv3 <gustavo.lopesv3@gmail.com>
2019-09-10 11:48:47 -0500

    autenticação para acessas modulo tarefas

--- app/views/tarefa_views.py
+++ app/views/tarefa_views.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from ..forms import TarefaForm
 from ..entidades.tarefa import Tarefa
 from ..services import tarefa_service
+
+@login_required()
 def listar_tarefas(request):
     tarefas = tarefa_service.listar_tarefas()
     return render(request, 'tarefas/listar_tarefas.html', {'tarefas': tarefas})
-
+@login_required()
 def cadastrar_tarefa(request):
     if request.method == 'POST':
         form_tarefa = TarefaForm(request.POST)
@@ -21,7 +24,7 @@ def cadastrar_tarefa(request):
     else:
         form_tarefa = TarefaForm()
     return render(request, 'tarefas/form_tarefa.html', {'form_tarefa': form_tarefa})
-
+@login_required()
 def editar_tarefa(request, id):
     tarefa_db = tarefa_service.listar_tarefas_id(id)
     form_tarefa = TarefaForm(request.POST or None, instance=tarefa_db)
@@ -35,7 +38,7 @@ def editar_tarefa(request, id):
         tarefa_service.editar_tarefa(tarefa_db, tarefa_nova)
         return redirect('listar_tarefas')
     return render(request, 'tarefas/form_tarefa.html', {'form_tarefa': form_tarefa})
-
+@login_required()
 def remover_tarefa(request, id):
     tarefa_db = tarefa_service.listar_tarefas_id(id)
     if request.method == 'POST':
